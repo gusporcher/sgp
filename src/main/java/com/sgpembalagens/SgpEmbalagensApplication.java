42468c9cfbf9922bd25ed5a5323b283ff9451b5d
Gustavo <gustavoporcher@gmail.com>
2020-04-09 10:53:56 -0300

    criacao do projeto e adicao de dependencias

---
+++ src/main/java/com/sgpembalagens/SgpEmbalagensApplication.java
@@ -0,0 +1,13 @@
+package com.sgpembalagens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgpEmbalagensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgpEmbalagensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sgpembalagens/SgpEmbalagensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgpembalagens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgpEmbalagensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
